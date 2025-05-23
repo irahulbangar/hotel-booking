fb903c9705bf8e9d0fccf560ab1062a147cceb64
Rahul Bangar <105964685+irahulbangar@users.noreply.github.com>
2025-05-23 19:35:43 +0530

    Add Footer, AllRooms page, and NewsLetter component; update App and Home pages

---
+++ src/pages/AllRooms.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllRooms = () => {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28">
+      <h1>All Rooms</h1>
+    </div>
+  );
+};
+
+export default AllRooms;
