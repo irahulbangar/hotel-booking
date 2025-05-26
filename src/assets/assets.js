3c2b6530e01ecab2554cdb91886d53eb088a08c7
Rahul Bangar <105964685+irahulbangar@users.noreply.github.com>
2025-05-26 19:08:12 +0530

    Add MyBookings and HotelReg components; update App to include new routes and enhance RoomDetails with amenities and booking form

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -177,7 +177,7 @@ export const userDummyData = {
   username: "Great Stack",
   email: "user.greatstack@gmail.com",
   image:
-    "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18yeFVVazQyTTFyYmQzMkhRU3o2SXJFN01XS3UifQ?width=160 2x",
   role: "hotelOwner",
   createdAt: "2025-03-25T09:29:16.367Z",
   updatedAt: "2025-04-10T06:34:48.719Z",
